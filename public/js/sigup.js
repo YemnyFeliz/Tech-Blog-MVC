96b8ea25a5727119dcd6e03a0403539be12f03e7
Yemny <y.feliz1296@gmail.com>
2023-11-11 20:04:07 -0500

    created new files in public/js and added code to signup.js

---
+++ public/js/sigup.js
@@ -0,0 +1,34 @@
+const signupFormHandler = async (event) => {
+    event.preventDefault();
+
+    const username = document.querySelector('username-signup').ariaValueMax.trim();
+    const password = document.querySelector('password-signup').ariaValueMax.trim();
+
+    if (!username || !password) {
+        alert('please provide email and password');
+    } else if (password.length < 8) {
+        alert('Password must be at least 8 charaters long, please try again');
+    }
+
+    if (username && password) {
+        try {
+            const response = await fetch('/api/users/signup', {
+                method: 'POST',
+                body: JSON.stringify({ username: username, password: password}),
+                headers: {'Content-Type': 'application/json'},
+            });
+            
+            if (response.ok) {
+                document.location.replace('/');
+            } else {
+                alert('Something went wrong, please try again!');
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    }
+};
+
+document
+.querySelector('.signup-form')
+.addEventListener('submit', signupFormHandler);
