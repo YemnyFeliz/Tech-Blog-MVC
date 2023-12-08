163cc9839176614af40a320305efa64bccabd59f
Yemny <y.feliz1296@gmail.com>
2023-12-08 08:56:19 -0500

    changed logout page back trying to get posts show in heroku web

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,22 +1,19 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const logout = async () => {
-        try {
-            const response = await fetch ('api/users/logout', {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' },
-            });
-            if (response.ok) {
-                document.location.replace('/');
-            } else {
-                alert ('Error logging out!');
-            }
-        } catch (error) {
-            console.error('Error:', error);
+const logout = async () => {
+    try {
+        const response = await fetch ('api/users/logout', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+        });
+        if (response.ok) {
+            document.location.replace('/');
+        } else {
+            alert ('Error logging out!');
         }
-    };
-
-    const logoutLink = document.querySelector('#logout-link');
-    if (logoutLink) {
-        logoutLink.addEventListener('click', logout);
+    } catch (error) {
+        console.error('Error:', error);
     }
-});
+};
+
+document
+.querySelector('#logout-link')
+.addEventListener('click', logout);
